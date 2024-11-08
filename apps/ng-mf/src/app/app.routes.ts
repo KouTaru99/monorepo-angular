c9279797a785138bc087b136bf744e6252ee86ac
sangnt <tansangnguyen@SangNT.local>
2024-11-08 23:54:52 +0700

    fix 404 nginx

--- apps/ng-mf/src/app/app.routes.ts
+++ apps/ng-mf/src/app/app.routes.ts
@@ -1,42 +1,13 @@
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
-  {
-    path: 'app-remote',
-    loadChildren: () => import('app-remote/Routes').then((m) => m.remoteRoutes),
-  },
-  {
-    path: 'dashboard',
-    component: NxWelcomeComponent,
-  },
-  {
-    path: 'products',
-    component: NxWelcomeComponent,
-  },
-  {
-    path: 'products/level1.1',
-    component: NxWelcomeComponent,
-  },
-  {
-    path: 'products/level2.1',
-    component: NxWelcomeComponent,
-  },
-  {
-    path: 'settings',
-    component: NxWelcomeComponent,
-  },
-  {
-    path: 'settings/customize',
-    component: NxWelcomeComponent,
-  },
-  {
-    path: 'settings/profile',
-    component: NxWelcomeComponent,
-  },
   {
     path: '',
     redirectTo: 'app-remote',
     pathMatch: 'full',
   },
+  {
+    path: 'app-remote',
+    loadChildren: () => import('app-remote/Routes').then((m) => m.remoteRoutes),
+  }
 ];
